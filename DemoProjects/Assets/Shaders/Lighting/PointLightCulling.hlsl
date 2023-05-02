8b1bf371dbc09a07de7ae8042acf13b8b102628a
Egor Timoshenko <cntc.et@gmail.com>
2023-05-03 01:22:50 +0400

    fixed point light tiling

--- DemoProjects/Assets/Shaders/Lighting/PointLightCulling.hlsl
+++ DemoProjects/Assets/Shaders/Lighting/PointLightCulling.hlsl
@@ -4,6 +4,7 @@
 #include "../Common/CameraData.hlsli"
 #include "../Common/CommonBindingTypes.hlsli"
 #include "../Common/Box.hlsli"
+#include "../Common/Type.hlsli"
 
 struct PointLightBindings
 {
@@ -34,6 +35,51 @@ bool checkIntersectsAABB(in float3 lightCenter, in float lightRadius, in AABB aa
 	return distSq <= lightRadius * lightRadius;
 }
 
+float2 calcLightProjPos(in float3 pos, in float4x4 proj)
+{
+	const float4 projPos = mul(float4(pos, 1.0f), proj);
+	return float2(projPos.xy / projPos.w);
+}
+
+struct LightProjBoundingBox
+{
+	float2 m_minPosition;
+	float2 m_maxPosition;
+};
+
+LightProjBoundingBox calcLightMinMaxProjPos(in float3 lightViewPosition, float lightRange, in float4x4 proj)
+{
+	float2 lightBoundBoxCorners[8];
+	lightBoundBoxCorners[0] = calcLightProjPos(float3(lightViewPosition.x - lightRange, lightViewPosition.y - lightRange, lightViewPosition.z - lightRange), proj);
+	lightBoundBoxCorners[1] = calcLightProjPos(float3(lightViewPosition.x - lightRange, lightViewPosition.y + lightRange, lightViewPosition.z - lightRange), proj);
+	lightBoundBoxCorners[2] = calcLightProjPos(float3(lightViewPosition.x - lightRange, lightViewPosition.y - lightRange, lightViewPosition.z + lightRange), proj);
+	lightBoundBoxCorners[3] = calcLightProjPos(float3(lightViewPosition.x - lightRange, lightViewPosition.y + lightRange, lightViewPosition.z + lightRange), proj);
+
+	lightBoundBoxCorners[4] = calcLightProjPos(float3(lightViewPosition.x + lightRange, lightViewPosition.y - lightRange, lightViewPosition.z - lightRange), proj);
+	lightBoundBoxCorners[5] = calcLightProjPos(float3(lightViewPosition.x + lightRange, lightViewPosition.y + lightRange, lightViewPosition.z - lightRange), proj);
+	lightBoundBoxCorners[6] = calcLightProjPos(float3(lightViewPosition.x + lightRange, lightViewPosition.y - lightRange, lightViewPosition.z + lightRange), proj);
+	lightBoundBoxCorners[7] = calcLightProjPos(float3(lightViewPosition.x + lightRange, lightViewPosition.y + lightRange, lightViewPosition.z + lightRange), proj);
+
+	float minX = FLT_MAX;
+	float minY = -FLT_MAX;
+	float maxX = -FLT_MAX;
+	float maxY = FLT_MAX;
+
+	for (uint i = 0; i < 8; ++i)
+	{
+		minX = min(minX, lightBoundBoxCorners[i].x);
+		minY = max(minY, lightBoundBoxCorners[i].y);
+		maxX = max(maxX, lightBoundBoxCorners[i].x);
+		maxY = min(maxY, lightBoundBoxCorners[i].y);
+	}
+
+	LightProjBoundingBox boundingBox;
+	boundingBox.m_minPosition = float2(minX, minY);
+	boundingBox.m_maxPosition = float2(maxX, maxY);
+
+	return boundingBox;
+}
+
 [numthreads(16, 1, 1)]
 void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
@@ -51,18 +97,12 @@ void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
 	RWStructuredBuffer<PointLightTileMask> tileMasks = ResourceDescriptorHeap[pointLightBindings.m_tileMaskBufferIndex];
 	
 	const PointLight light = pointLights[lightIndex];
-
 	const float4 lightViewPosition = mul(float4(light.m_position, 1.0f), cameraTransforms.m_viewTransformMatrix);
-	const float4 lightRangeDir = float4(light.m_range, -light.m_range, light.m_range, 0.0f);
 
-	const float4 minLightViewPosition = lightViewPosition - lightRangeDir;
-	const float4 maxLightViewPosition = lightViewPosition + lightRangeDir;
+	const LightProjBoundingBox lightProjBoundingBox = calcLightMinMaxProjPos(lightViewPosition.xyz, light.m_range, cameraTransforms.m_projTransformMatrix);
 
-	const float4 minLightProjPosition = mul(minLightViewPosition, cameraTransforms.m_projTransformMatrix);
-	const float4 maxLightProjPosition = mul(maxLightViewPosition, cameraTransforms.m_projTransformMatrix);
-
-	const float2 normMinLightScreenPosition = (minLightProjPosition.xy / minLightProjPosition.w + float2(1.0f, 1.0f)) * 0.5f;
-	const float2 normMaxLightScreenPosition = (maxLightProjPosition.xy / maxLightProjPosition.w + float2(1.0f, 1.0f)) * 0.5f;
+	const float2 normMinLightScreenPosition = (lightProjBoundingBox.m_minPosition + float2(1.0f, 1.0f)) * 0.5f;
+	const float2 normMaxLightScreenPosition = (lightProjBoundingBox.m_maxPosition + float2(1.0f, 1.0f)) * 0.5f;
 
 	const float2 invertNormMinLightScreenPosition = float2(normMinLightScreenPosition.x, 1.0f - normMinLightScreenPosition.y);
 	const float2 invertNormMaxLightScreenPosition = float2(normMaxLightScreenPosition.x, 1.0f - normMaxLightScreenPosition.y);
@@ -75,12 +115,12 @@ void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
 
 	const float2 normTileSize = float2(1.0f / lightVolume.m_tilesCountX, 1.0f / lightVolume.m_tilesCountY);
 
-	const uint2 minLightTileCoords = trunc(saturate(invertNormMinLightScreenPosition) / normTileSize);
+	const uint2 minLightTileCoords = floor(saturate(invertNormMinLightScreenPosition) / normTileSize);
 	const uint2 maxLightTileCoords = ceil(saturate(invertNormMaxLightScreenPosition) / normTileSize);
 
-	for (uint y = minLightTileCoords.y; y <= maxLightTileCoords.y; ++y)
+	for (uint y = minLightTileCoords.y; y < maxLightTileCoords.y; ++y)
 	{
-		for (uint x = minLightTileCoords.x; x <= maxLightTileCoords.x; ++x)
+		for (uint x = minLightTileCoords.x; x < maxLightTileCoords.x; ++x)
 		{
 			const uint tileIndex = y * lightVolume.m_tilesCountX + x;
 
